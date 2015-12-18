9a67944d8084a5df1854181b31bdcdd646e70056
Atom Powers <atom.powers@gmail.com>
2015-12-18 13:29:18 -0800

    cleanup sublime conf files

--- setup_sublime.sh
+++ setup_sublime.sh
@@ -4,4 +4,4 @@ add-apt-repository -y ppa:webupd8team/sublime-text-3
 apt-get install sublime-text
 
 # install configs for sublime
-cp sublime/* ~/.config/sublime-text-2/Packages/User/.
+cp sublime/* ~/.config/sublime-text-3/Packages/User/.
